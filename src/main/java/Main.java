69f5560db1c617dc79a015efd567cd204db9e967
Launois Remy <remy.launois@gmail.com>
2020-05-05 17:13:20 +0200

    Revert "Fonctionne"
    This reverts commit b5e43c79988be4f4148a1f1c692d99cae1f6afba.
    
    # Conflicts:
    #	src/main/java/Main.java
    #	src/main/java/fable/GzipExtract.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,31 +2,25 @@ import Downloader.Downloader;
 import fable.FileToGzip;
 import fable.GzipExtract;
 
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class Main {
 
-    public static void main(String[] args) throws URISyntaxException {
-        //FileToGzip ftgUrl = new FileToGzip();
+    public static void main(String[] args) {
         FileToGzip ftg = new FileToGzip();
-        //Downloader d = new Downloader("https://www.gutenberg.org/files/11339/11339-8.txt");
-        //d.download("./src/main/resources/fablesUrl.txt");
-        URL url = Main.class.getClassLoader().getResource("fables.txt");
-        Path path = Paths.get(url.toURI());
+        /*FileToGzip ftgUrl = new FileToGzip();
+        Downloader d = new Downloader("https://www.gutenberg.org/files/11339/11339-8.txt");
+        d.download("./src/main/resources/fablesUrl.txt");*/
+
         ftg.fileToGzip("fables.txt","fables.gz",0);
         //ftgUrl.fileToGzip("fablesUrl.txt","fablesUrl.gz",1);
 
         GzipExtract ge = new GzipExtract();
         System.out.println("With name ===============================================================");
         //ge.readWithName("fablesUrl.gz","THE GOATHERD AND THE WILD GOATS");
-        ge.readWithName(path,"fables.gz","THE GOATHERD AND THE WILD GOATS");
+        ge.readWithName("fables.gz","THE GOATHERD AND THE WILD GOATS");
 
         System.out.println("With id ===============================================================");
         //ge.readWithId("fablesUrl.gz",1);
-        ge.readWithId(path,"fables.gz",1);
+        ge.readWithId("fables.gz",1);
 
 
     }
